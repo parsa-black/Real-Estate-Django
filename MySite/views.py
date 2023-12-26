85be23dcdf3987c24e4c57e074d89c46b7f19f60
parsa <parsagorgzan1225@gmail.com>
2023-12-26 23:45:25 +0330

    add Forms and Update HTML

--- MySite/views.py
+++ MySite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse
 from .models import Property
+from .froms import UserForm
 
 
 def homepage(request):
@@ -19,6 +20,13 @@ def list_house(request):
 
 def register(request):
     if request.method == 'POST':
-        print(request.POST)
-        return redirect(reverse('home-page'))
-    return render(request, 'register.html', {})
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            print(user_form.cleaned_data)
+            return redirect(reverse('home-page'))
+    else:
+        user_form = UserForm()
+
+    return render(request, 'register.html', {
+        'user_form': user_form
+    })
