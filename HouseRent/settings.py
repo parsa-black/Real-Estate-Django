0038427a1cf4bb9c22cfe557fb6ae33bff56be01
AminCht <amincht81@gmail.com>
2023-12-27 00:28:00 +0330

    igonre

--- HouseRent/settings.py
+++ HouseRent/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -68,6 +69,11 @@ TEMPLATES = [
     },
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 WSGI_APPLICATION = 'HouseRent.wsgi.application'
 
 
@@ -81,7 +87,7 @@ DATABASES = {
         'HOST': 'localhost',
         'USER': 'root',
         'PASSWORD': '1535',
-        'PORT': '3306',
+        'PORT': '3307',
     }
 }
 
