e1941d8c9bd0358f89726f26cb56826a81415f5a
AminCht <amincht81@gmail.com>
2023-12-27 03:39:11 +0330

    add login view

--- MySite/views.py
+++ MySite/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Property
-from .froms import UserForm
+from .models import Property, Users
+from .forms import LoginForm, UserForm
+from django.contrib.auth.hashers import make_password, check_password
+
+
 
 
 def homepage(request):
@@ -18,14 +22,46 @@ def list_house(request):
 
 
 def register(request):
+    print(request.method)
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         if user_form.is_valid():
-            user_form.save()
+            user = user_form.save(commit=False)
+            hashed_password = make_password(user_form.cleaned_data['password'])  # Create a user object but don't save it yet
+            user.password = hashed_password
+            user.save()
             return redirect('home-page')
     else:
         user_form = UserForm()
-
     return render(request, 'register.html', {
         'user_form': user_form
     })
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home-page')
+
+    form = LoginForm(request.POST or None)
+    print(request.method)
+    msg = None
+
+    if request.method == 'POST':
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            try:
+                user = Users.objects.get(username=username)
+            except Users.DoesNotExist:
+                user = None
+            if user is not None:
+                password_matches = check_password(password, user.password)
+                if password_matches: 
+                    return redirect('home-page')
+                msg= 'Invalid credentials'
+            else:
+                msg = 'Invalid credentials'
+        else:
+            msg = 'Error validating the form'
+
+    return render(request, 'login.html', {'form': form, 'msg': msg})
