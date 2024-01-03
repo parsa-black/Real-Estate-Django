3f937b80e6db503e0ed3c2a3aed5cd2b776d1104
parsa <parsagorgzan1225@gmail.com>
2024-01-03 04:46:28 +0330

    Auth Owner to Submit House

--- MySite/views.py
+++ MySite/views.py
@@ -74,15 +74,19 @@ def login_view(request):
 
 @login_required()
 def property_register(request):
-    if request.method == 'POST':
-        property_form = PropertyForm(request.POST)
-        if property_form.is_valid():
-            property_form.instance.house_owner = request.user
-            property_form.save()
-            return redirect('home-page')
-    else:
-        property_form = PropertyForm()
+    if request.user.profileuser.role == 'O':
+        if request.method == 'POST':
+            property_form = PropertyForm(request.POST)
+            if property_form.is_valid():
+                property_instance = property_form.save(commit=False)
+                property_form.instance.house_owner = request.user.profileuser
+                property_form.save()
+                return redirect('home-page')
+        else:
+            property_form = PropertyForm()
 
-    return render(request, 'propertyRegister.html', {
-        'property_form': property_form
-    })
+        return render(request, 'propertyRegister.html', {
+            'property_form': property_form
+        })
+    else:
+        return render(request, 'access_denied.html')
