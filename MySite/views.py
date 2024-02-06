af106e59aa3547c9f1ee7bd5cebdf8c48b9ee92e
parsa <parsagorgzan1225@gmail.com>
2024-02-06 22:22:29 +0330

    View Clean

--- MySite/views.py
+++ MySite/views.py
@@ -1,12 +1,9 @@
-# from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
-from django.urls import reverse
 
 from MySite.dto import Dto
-from .models import Property, Review, Document, ProfileUser
+from .models import Property, Review, Document
 from .forms import LoginForm, UserForm, ProfileForm, PropertyForm, ReviewForm, DocumentForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.hashers import make_password
@@ -16,7 +13,6 @@ from django.db.models import Q
 
 def homepage(request):
     properties = Property.objects.select_related('house_owner').all()
-    # owner_phone = ProfileUser.objects.filter(id=request.user.profileuser.id).values_list('phone_number')
     return render(request, 'home.html', {'properties': properties})
 
 
@@ -26,7 +22,6 @@ def search_view(request):
     properties = None
     if query:
         properties = Property.objects.select_related('house_owner').filter(title__icontains=query)
-        # owner_phone = ProfileUser.objects.filter(id=request.user.profileuser.id).values_list('phone_number')
         context = {'properties': properties, 'query': query, 'msg': msg}
         return render(request, 'home.html', context)
     else:
@@ -38,8 +33,9 @@ def search_view(request):
 
 def property_view(request, property_id):
     prop = Property.objects.select_related('house_owner').get(id=property_id)
-    # owner_phone = ProfileUser.objects.filter(id=request.user.profileuser.id).values_list('phone_number')
-    dto = Dto(prop.title, prop.description, prop.rent_price, prop.house_city, prop.house_address, prop.bedrooms, prop.bathrooms, prop.area, prop.yard_area, prop.year, prop.garage, prop.house_review, prop.house_owner.phone_number)
+    dto = Dto(prop.title, prop.description, prop.rent_price, prop.house_city, prop.house_address, prop.bedrooms,
+              prop.bathrooms, prop.area, prop.yard_area, prop.year, prop.garage, prop.house_review,
+              prop.house_owner.phone_number)
     dto.convertBooleanToString(dto)
     return render(request, 'property.html', {'prop': dto})
 
@@ -74,7 +70,7 @@ def register(request):
                 profile.save()
                 return redirect('home-page')
             else:
-                msg = 'Password sould be equal to password confirm'
+                msg = 'Password should be equal to password confirm'
     else:
         user_form = UserForm()
         profile_form = ProfileForm()
@@ -146,7 +142,7 @@ def review_submit(request, property_id):
                     review.tenant = request.user.profileuser
                     review.property = prop
                     review.save()
-                    sweetify.success(request, 'Your review have been Submited')
+                    sweetify.success(request, 'Your review have been Submitted')
                     return redirect('home-page')
             else:
                 review_form = ReviewForm()
